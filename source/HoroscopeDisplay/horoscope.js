c00d894503ec9bc3f6951ee11e5084a6bed118a8
niknak1379 <nikanostovan@gmail.com>
2023-05-26 19:57:42 -0700

    Changed horoscope.html homepage functionality

--- source/HoroscopeDisplay/horoscope.js
+++ source/HoroscopeDisplay/horoscope.js
@@ -1,66 +1,92 @@
-//const sanitizer = new Sanitizer();
+var birthday; //birthday input, got from local storage
+var horoscopeArray; //array including the horoscope responses
+var homeButton; //button used to go back to the home page
 
-var birthday;
-var horoscopeArray;
-var submitButton;
-
+//init function on load of the page
 window.addEventListener("load", init);
+
+/*
+ *init function used to fetch horoscope.json file and set up the home page
+ *event listener behavior. Exit code 1 if successful, 0 if not.
+ */
 async function init(){
+
     try{
+
+        //fetch the horoscope json file and convert to an array
+        //containing the responses
         horoscopeArray = await fetch('./horoscope.JSON');
         horoscopeArray = await horoscopeArray.json();
         horoscopeArray = horoscopeArray['data'];
-        //submitButton = document.getElementById("submitButton");
 
-        //set birthday and name for testing purposes
-        console.log(localStorage.getItem('UserName'));
+        //get homeButton and set up event listener
+        homeButton = document.getElementById("homeButton");
+        homeButton.addEventListener('click', goHome);
+
+        //output the horoscope based on the birthday of the user
         outputHoroscope();
+
+        //exit code
         return 1;
     }
+
+    //catch errors  with exit code 0
     catch(error){
         console.error(error);
         return 0;
     }
 }
+
+/*
+ *expamle test function that returns the input
+ */
 function exampleTest(num){
     return num;
 }
+
+
 /*
  * Based on the input name and birthday, a horoscope reading
  * will be outputted to the user 
  */
 function outputHoroscope() {
-    //event.preventDefault();
-    console.log("output");
 
     //get birthday and name from local storage
     let customerBirthday = localStorage.getItem('Birthday');
     let customerName = localStorage.getItem('UserName');
+
+    //set up birthdate in correct format and get numerical month
     birthday = new Date(customerBirthday);
-
-    // Create a new Date object
-    let todayDate = new Date();
-
-    // Get the numerical day of the month
-    let todayDay = todayDate.getDate();
     let birthdayMonth = birthday.getMonth();
+
+    // get the current numereical day of the month for today
+    let todayDate = new Date();
+    let todayDay = todayDate.getDate();
+    
+    //randomize the responese of the horroscope by using the date of today to offset
+    //the resopnse of the array
     let hashValue = (todayDay+birthdayMonth)%12;
     console.log(hashValue);
 
-   
-    
+    //get output elements from the horoscope.html file
     let horoscopeOutput = document.getElementById('horoscopeOutput');
     let nameOutput = document.getElementById('fname');
     let birthdayOutput = document.getElementById('birthday');
     let zodiacOutput = document.getElementById('zodiacSign');
+
+    //display the output in the correct element
     nameOutput.innerHTML = customerName;
     birthdayOutput.innerHTML = customerBirthday;
     zodiacOutput.innerHTML = horoscopeArray[birthdayMonth]['sign'];
-    console.log(horoscopeArray[hashValue]['horoscope']);
-    console.log(horoscopeArray[birthdayMonth]['sign']);
     horoscopeOutput.innerHTML=`${horoscopeArray[hashValue]['horoscope']}`;  
-    
 } 
 
-//console.log(submitButton);
+/*
+ * changes the location fo the window to the home page
+ */
+function goHome() {
+    window.location.href = "../../index.html"; 
+}
+
+//module export statement used for JEST testing
 module.exports = exampleTest;
